4d1f87bff2e8208b56f6ae0373890cdd81b8900b
Szczepan Zalega <szczepan@nitrokey.com>
2019-07-27 11:40:52 +0200

    Add information about taken log

--- start-test-suite.sh
+++ start-test-suite.sh
@@ -7,3 +7,6 @@ pcscd
 cd nitrokey-test-suite-master || exit
 # env GPG_TTY=$(tty) 
 behave -s --no-capture --no-capture-stderr  -x
+
+echo "File ./scdaemon.log contains the current log of GnuPG communication with the smart card device."
+
