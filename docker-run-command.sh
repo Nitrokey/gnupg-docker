381cf36a7f4563996f98c1b55cd924f0581ed784
Szczepan Zalega <szczepan@nitrokey.com>
2020-01-21 12:13:41 +0100

    Remove obsolete code

--- docker-run-command.sh
+++ docker-run-command.sh
@@ -1,19 +1,9 @@
 #!/usr/bin/env bash
 
 if [ "$EUID" != "0" ]; then
-    echo "Run docker image with production tool"
-    echo === Disabling automounting on Ubuntu 16.04
-    gsettings set org.gnome.desktop.media-handling automount false
-    echo === To enable back automounting use:
-    echo === gsettings set org.gnome.desktop.media-handling automount true
-    echo
-
     echo "Running script with root privileges"
 	exec sudo bash $0 $@
 fi
-
-echo === Enabling PC Speaker
-modprobe pcspkr
 
 echo === Closing working pcscd on host
 killall pcscd scdaemon
